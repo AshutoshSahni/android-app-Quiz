57bae95f58760d1914b43b1453e0fde5d843cea1
Ashutosh Sahni <iashutoshsahni@gmail.com>
2023-02-10 16:22:57 +0530

    created image files for correct ( correct_option_border_bg.xml ) and wrong ( wrong_option_border_bg.xml ) options updated QuizQuestionActivity.kt so that options, on selection change their default appearance

--- app/src/main/java/com/ashutoshsahni/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/ashutoshsahni/quizapp/QuizQuestionActivity.kt
@@ -1,14 +1,21 @@
 package com.ashutoshsahni.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mQuestionList : ArrayList<Question>? = null
+    private var mSelectedOption : Int = 0
+    private var mCurrentPosition : Int = 1
 
     private var progressBar : ProgressBar? = null
     private var tvProgressBar : TextView? = null
@@ -19,6 +26,8 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     private var optionTwo : TextView? = null
     private var optionThree : TextView? = null
     private var optionFour : TextView? = null
+
+    private var btnSubmit : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,28 +43,91 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         optionThree = findViewById(R.id.tv_optionThree)
         optionFour = findViewById(R.id.tv_optionFour)
 
+        btnSubmit = findViewById(R.id.btnSubmit)
 
-        val questionList = Constants.getQuestion()
-        Log.i("Size of question list is : ", "${questionList.size}")
+        optionOne?.setOnClickListener(this)
+        optionTwo?.setOnClickListener(this)
+        optionThree?.setOnClickListener(this)
+        optionFour?.setOnClickListener(this)
 
-        for(i in questionList) {
-            Log.e("Questions: ", i.question)
+        btnSubmit?.setOnClickListener(this)
+
+        mQuestionList = Constants.getQuestion()
+        setQuestion()
+    }
+
+    private fun defaultOptionsView() {
+        val optionList = ArrayList<TextView>()
+
+        optionOne?.let { optionList.add(0, it) }
+        optionTwo?.let { optionList.add(1, it) }
+        optionThree?.let { optionList.add(2, it) }
+        optionFour?.let {
+            optionList.add(3, it)
         }
 
-        var currentPosition = 1
-        val question : Question = questionList[currentPosition - 1]
+        for (option in optionList) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.option_border_bg
+            )
+        }
+
+    }
+
+    // for selected option style
+    private fun selectedOptionView(tv : TextView, selectedOption: Int) {
+        defaultOptionsView()
+
+        mSelectedOption = selectedOption
+        tv.setTextColor(Color.parseColor("#363A43" ))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    private fun setQuestion() {
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
         ivImage?.setImageResource(question.image)
-        progressBar?.progress = currentPosition
-        tvProgressBar?.text = "$currentPosition/${progressBar?.max}"
+        progressBar?.progress = mCurrentPosition
+        tvProgressBar?.text = "$mCurrentPosition/${progressBar?.max}"
         tvQuestion?.text = question.question
         optionOne?.text = question.optionOne
         optionTwo?.text = question.optionTwo
         optionThree?.text = question.optionThree
         optionFour?.text = question.optionFour
 
+        if (mCurrentPosition == mQuestionList!!.size)
+            btnSubmit?.text = "FINISH"
+        else
+            btnSubmit?.text = "SUBMIT"
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.tv_optionOne -> optionOne?.let {
+                selectedOptionView(it, 1)
+            }
+
+            R.id.tv_optionTwo -> optionTwo?.let {
+                selectedOptionView(it, 2)
+            }
+
+            R.id.tv_optionThree -> optionThree?.let {
+                selectedOptionView(it, 2)
+            }
+
+            R.id.tv_optionFour -> optionFour?.let {
+                selectedOptionView(it, 4)
+            }
+
+            R.id.btnSubmit -> {
+                // TODO: need to work on submit button
+            }
+        }
     }
 }
